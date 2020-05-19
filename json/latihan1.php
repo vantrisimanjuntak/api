3d2abac200f192bdfb5de615ef2d0e0dd4895fba
Vantri Simanjuntak <vantri_antonius@yahoo.co.id>
2020-05-19 19:22:37 +0700

    API

---
+++ json/latihan1.php
@@ -0,0 +1,15 @@
+<?php
+
+$mahasiswa = [
+    [
+        'nama' => "Vantri Simanjuntak",
+        'nim' => "16030081"
+    ],
+    [
+        'nama' => "Vantri Simanjuntak",
+        'nim' => "16030081"
+    ]
+];
+
+$data = json_encode($mahasiswa);
+echo $data;
---
+++ json/script.js
@@ -0,0 +1,6 @@
+let mahasiswa = {
+    nama: "Vantri Simanjuntak",
+    nim: 16030081,
+    email: "vantri@stta.ac.id"
+}
+console.log(JSON.stringify(mahasiswa));
